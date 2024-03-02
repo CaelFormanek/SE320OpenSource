8a23a948d2796853167cb78e5646b7e7efa79d5e
Cael Formanek <cformanek@chapman.edu>
2024-03-02 13:34:58 -0800

    added initial files for questions and options

---
+++ Question.cpp
@@ -0,0 +1,21 @@
+#include "Question.h"
+
+Question::Question(std::string option1Str, std::string option2Str, std::string option3Str, std::string option4Str, int correctAnswerID)
+{
+  this->option1 = new Option(option1Str, 1);
+  this->option2 = new Option(option2Str, 2);
+  this->option3 = new Option(option3Str, 3);
+  this->option4 = new Option(option4Str, 4);
+  this->correctAnswerID = correctAnswerID;
+
+  optionsarr = new Option*[4];
+  optionsarr[0] = option1;
+  optionsarr[1] = option2;
+  optionsarr[2] = option3;
+  optionsarr[3] = option4;
+}
+
+Option* Question::getOption(int optionID)
+{
+  return optionsarr[optionID-1];
+}
---
+++ Question.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Option.h"
+#include <string>
+
+class Question
+{
+  
+  private:
+    Option* option1; // question answer 1
+    Option* option2; // question answer 2
+    Option* option3; // question answer 3
+    Option* option4; // question answer 4
+    int correctAnswerID; // ID for the correct answer
+    Option** optionsarr; // array that holds the answers
+    
+  public:
+    Question(std::string option1Str, std::string option2Str, std::string option3Str, std::string option4Str, int correctAnswerID);
+    ~Question();
+    Option* getOption(int optionID);
+};
