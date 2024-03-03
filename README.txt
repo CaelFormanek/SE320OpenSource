2ad07736fbbbe62f47bea8a70cab7b0b3e3c9150
Cael Formanek <cformanek@chapman.edu>
2024-03-03 15:34:31 -0800

    full testing suite

--- README.txt
+++ README.txt
@@ -6,11 +6,16 @@ potential answers to a quiz question), a Question (the actual question
 being asked), and a StudyGame (a game for the user to play).
 
 Instructions for Running:
-g++ *.cpp -o source
+For running tests:
+g++ -std=c++17 -Igoogletest/googletest/include -Lgoogletest/build/lib -lgtest -lgtest_main *.cpp -o test_executable
+./test_executable
+
+For running regularly
+g++ -std=c++17 *.cpp -o source
 ./source
 
 List of Five Issues to Fix:
-1. The getOption() method in Question.cpp will sometimes cause the program to quit. Fix this bug so that the error is handled.
+1. The getOption() method in Question.cpp can access options outside the given range. Fix this bug so that the error is handled.
 2. Make is so that the number of Questions in a StudyGame can change (the array can resize dynamically). This means that if a user defines a StudyGame with one question, but they decide to add a second, this will work.
 3. When the program is run, it will ask the user if they want to press 1 or 2 to continue or quit. What happens if a user puts "banana"? Implement this error handling.
 4. Add an option for users to shuffle the order in which quiz questions are asked
--- main.cpp
+++ main.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
+#include <gtest/gtest.h>
 #include "StudyGame.h"
 
-int main(int argc, char** argv)
+// test that the program exits upon illegal reference
+TEST(GetOptionTest, TestDeath)
+{
+  Question q("Alaska", "Rhode Island", "South Dakota", "Louisiana", 1, "Which state is the largest");
+  ASSERT_DEATH(q.getOption(-1), "");
+}
+
+// test that a user can enter anything and the game will exit fine
+// to run this test, type "banana" when prompted to enter 1 or 2
+TEST(UserOptionTest, TestDeathInput)
+{
+  Question* q = new Question("Alaska", "Rhode Island", "South Dakota", "Louisiana", 1, "Which state is the largest");
+  
+  StudyGame* s = new StudyGame(1);
+  s->addQuestion(q);
+
+  s->play();
+}
+
+// test that issue 5 does not happen
+TEST(AddQuestionTest, TestQuestion)
 {
   Question* q = new Question("Alaska", "Rhode Island", "South Dakota", "Louisiana", 1, "Which state is the largest");
   Question* q1 = new Question("beep", "beep", "glorp", "norb", 3, "Which word is the coolest");
-  
+
   StudyGame* s = new StudyGame(2);
   s->addQuestion(q);
   s->addQuestion(q1);
 
-  s->play();
-  
+  EXPECT_NE(s->questionsarr[0], s->questionsarr[1]);
+}
+
+// test that issue 2 is fixed
+TEST(TestDynamic, TestDyn)
+{
+  Question* q = new Question("Alaska", "Rhode Island", "South Dakota", "Louisiana", 1, "Which state is the largest");
+  Question* q1 = new Question("beep", "beep", "glorp", "norb", 3, "Which word is the coolest");
+
+  StudyGame* s = new StudyGame(1);
+  s->addQuestion(q);
+  s->addQuestion(q1);
+
+  EXPECT_NE(s->questionsarr[1], nullptr);
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  // Run tests
+  return RUN_ALL_TESTS();
+
   return 0;
 }
