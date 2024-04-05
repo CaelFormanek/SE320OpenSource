24489a3a74e858d11f495c343cf174fa1a3ff7de
Cael Formanek <cformanek@chapman.edu>
2024-04-05 11:39:13 -0700

    return to testing state

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int run_all_tests(Tester* t)
   // test for issue 2
   Question* q = new Question("Alaska", "Rhode Island", "South Dakota", "Louisiana", 1, "Which state is the largest");
   Question* q1 = new Question("beep", "beep", "glorp", "norb", 3, "Which word is the coolest");
-  StudyGame* s = new StudyGame(1);
-  s->addQuestion(q);
-  s->addQuestion(q1);
-  int test2 = t->assertEq("Test Question Count Dynamic", q1, s->questionsarr[1]);
-  if (test2 == -1)
-  {
-    // returnval = -1;
-  }
+  // StudyGame* s = new StudyGame(1);
+  // s->addQuestion(q);
+  // s->addQuestion(q1);
+  // int test2 = t->assertEq("Test Question Count Dynamic", q1, s->questionsarr[1]);
+  // if (test2 == -1)
+  // {
+  //   returnval = -1;
+  // }
 
   // test for issue 5
   StudyGame* s1 = new StudyGame(2);
@@ -25,17 +25,17 @@ int run_all_tests(Tester* t)
   int test5 = t->assertEq("Test addQuestion()", q1, s1->questionsarr[1]);
   if (test5 == -1)
   {
-    // returnval = -1;
+    returnval = -1;
   }
 
   // test for issue 1
-  Option* o = new Option("North Dakota", 5);
-  q->addOption(o);
-  int test1 = t->assertEqInt("Test addOption() 1", 5, q->getNumOptions());
-  if (test1 == -1)
-  {
-    // returnval = -1;
-  }
+  // Option* o = new Option("North Dakota", 5);
+  // q->addOption(o);
+  // int test1 = t->assertEqInt("Test addOption() 1", 5, q->getNumOptions());
+  // if (test1 == -1)
+  // {
+  //   returnval = -1;
+  // }
 
   return returnval;
 }
